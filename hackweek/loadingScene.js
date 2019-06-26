17917377b0bf435334ba439d1cdbd80e0ae540bd
Anna <asikkink@hawaii.edu>
2019-06-26 08:05:40 -1000

    whopps

--- hackweek/loadingScene.js
+++ hackweek/loadingScene.js
@@ -11,7 +11,7 @@ class loadingScene extends Phaser.Scene{
 		this.background = this.add.image(0,0,"background");
 		this.background.setOrigin(0,0);
 		
-		this.msg1 = this.add.image(config.width/2, config.height/2, "test-msg");
+		this.msg1 = this.add.image(800, 800, "test-msg");
 	}
 	
 }
