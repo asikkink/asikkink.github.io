19f292fd01398a79ab0f091e568d77157c77fde5
Anna <asikkink@hawaii.edu>
2019-06-26 08:09:48 -1000

    testing centering canvas

--- hackweek/loadingScene.js
+++ hackweek/loadingScene.js
@@ -11,7 +11,11 @@ class loadingScene extends Phaser.Scene{
 		this.background = this.add.image(0,0,"background");
 		this.background.setOrigin(0,0);
 		
-		this.msg1 = this.add.image(800, 800, "test-msg");
+		this.msg1 = this.add.image(config.width/2, config.height/2, "test-msg");
+		
+		game.scale.pageAlignHorizontally = true;
+		game.scale.pageAlignVertically = true;
+		game.scale.refresh();
 	}
 	
 }
